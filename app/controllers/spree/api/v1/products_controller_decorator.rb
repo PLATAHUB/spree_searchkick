2d695f74ff9605ac3e8577b65812f6c1dd6abbc7
luiscarlosplataforma <luiscarlos@civico.com>
2021-03-04 15:57:46 -0500

    FIX: error prepend

--- app/controllers/spree/api/v1/products_controller_decorator.rb
+++ app/controllers/spree/api/v1/products_controller_decorator.rb
@@ -1,23 +1,29 @@
-module Spree::Api::V1::ProductsControllerDecorator
-  include Spree::Core::ControllerHelpers::Search
+module Spree
+  module Api
+    module V1
+      module ProductsControllerDecorator
+        include ::Spree::Core::ControllerHelpers::Search
 
-  # Sort by conversions desc
-  def index
-    
-    @products = if params[:ids]
-      product_scope.where(id: params[:ids].split(',').flatten)
-    else
-      @searcher = build_searcher(params.merge(include_images: true, current_store_id: current_store.id))
-      @searcher.retrieve_products
+        # Sort by conversions desc
+        def index
+          
+          @products = if params[:ids]
+            product_scope.where(id: params[:ids].split(',').flatten)
+          else
+            @searcher = build_searcher(params.merge(include_images: true, current_store_id: current_store.id))
+            @searcher.retrieve_products
+          end
+
+          # @products = @products.distinct.page(params[:page]).per(params[:per_page])
+          expires_in 15.minutes, public: true
+          headers['Surrogate-Control'] = "max-age=#{15.minutes}"
+          respond_with(@products)
+        end
+      end
     end
-
-    # @products = @products.distinct.page(params[:page]).per(params[:per_page])
-    expires_in 15.minutes, public: true
-    headers['Surrogate-Control'] = "max-age=#{15.minutes}"
-    respond_with(@products)
   end
 end
   
 if defined?(Spree::Api::V1::ProductsController)
-  Spree::ProductsController.prepend(Spree::Api::V1::ProductsControllerDecorator)
+  ::Spree::Api::V1::ProductsController.prepend(Spree::Api::V1::ProductsControllerDecorator)
 end
